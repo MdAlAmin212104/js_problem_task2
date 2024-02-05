cded774026edb404adfba94094a74b940aec9b0d
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-05 19:23:02 +0600

    4th problem solved

--- js-problem2.js
+++ js-problem2.js
@@ -63,7 +63,7 @@ function calculateElectronicsBudget(laptop, tablet, mobile){
       return laptop*laptopPrice + tablet*tabletPrice + mobile*mobilePrice;
 }
 const totalBudget = calculateElectronicsBudget(2, 4, 7);
-console.log(totalBudget);
+//console.log(totalBudget);
 
 
 /*
@@ -85,7 +85,27 @@ const phones = [
 ];
 
 */
-
+const phones = [
+      { model: "PhoneA", brand: "Iphone", price: 95000 },
+      { model: "PhoneB", brand: "Samsung", price: 40000 },
+      { model: "PhoneC", brand: "Oppo", price: 26000 },
+      { model: "PhoneD", brand: "Nokia", price: 35000 },
+      { model: "PhoneE", brand: "Iphone", price: 105000 },
+      { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+function findAveragePhonePrice(phones){
+      let sum = 0;
+      let products = phones.length;
+      for(const ph of phones){
+            const price = ph.price;
+            sum += price;
+      }
+      // console.log(sum);
+      // console.log(products);
+      const average = (sum / products).toFixed(0);
+      return average;
+}
+console.log(findAveragePhonePrice(phones));
 
 
 /*
